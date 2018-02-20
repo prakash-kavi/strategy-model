eaeeee992381cf89da6ae5617d626f860c8617dd
Prakash Kavi <prakash.kavi@gmail.com>
2018-02-20 01:04:36 -0600

    Schema Validator: User

--- server/server.js
+++ server/server.js
@@ -1,17 +1,1 @@
-const SimpleSchema = require('mongoose');
-
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    max: 40,
-  },
-  age: {
-    type: SimpleSchema.Integer,
-    optional: true,
-  },
-  registered: {
-    type: Boolean,
-    defaultValue: false,
-  },
-});
 
