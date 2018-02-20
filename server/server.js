a5519a2246bfb4a0bb848e7a8fd611151b78e0a4
Prakash Kavi <33811837+prakash-kavi@users.noreply.github.com>
2018-02-19 19:45:01 -0600

    Server commit

--- server/server.js
+++ server/server.js
@@ -1,17 +1,2 @@
-const SimpleSchema = require('mongoose');
 
-const UserSchema = new Schema({
-  name: {
-    type: String,
-    max: 40,
-  },
-  age: {
-    type: SimpleSchema.Integer,
-    optional: true,
-  },
-  registered: {
-    type: Boolean,
-    defaultValue: false,
-  },
-});
 
